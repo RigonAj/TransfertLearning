5aac7d4b76e0a9060fd4418717f5a6b923c0f4ba
jenortier <jerome.nortier@etu.uca.fr>
2026-05-19 15:28:07 +0200

    Increase target segments from 10,000 to 100,000

--- agents/transfer/trajectories.py
+++ agents/transfer/trajectories.py
@@ -160,7 +160,7 @@ def record_one_segment(env2, env3, start_target, end_target,
 # =========================================================
 def main():
     NUM_SAMPLES = 50                     # <-- longueur fixe après SS
-    N_SEGMENTS_TARGET = 10_000
+    N_SEGMENTS_TARGET = 100_000
     TARGETS_PER_EPISODE = 30
     MAX_STEPS_PER_SEGMENT = 60
     TOLERANCE = 0.2
